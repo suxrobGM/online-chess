2bacc3c259761b049e9fd1d419f079a31c007e5d
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-03-27 18:07:27 -0400

    fix tests

--- backend/src/test/java/com/silyosbekov/chessmate/dto/CreateGameDtoTest.java
+++ backend/src/test/java/com/silyosbekov/chessmate/dto/CreateGameDtoTest.java
@@ -7,32 +7,36 @@ import static org.junit.jupiter.api.Assertions.*;
 class CreateGameDtoTest {
     @Test
     void createGameDtoShouldInitializeWithGivenValues() {
+        var gameId = "gameId";
         var whitePlayerId = "whitePlayerId";
         var blackPlayerId = "blackPlayerId";
-        var createGameDto = new NewGameDto(whitePlayerId, blackPlayerId);
+        var createGameDto = new NewGameDto(gameId, whitePlayerId, blackPlayerId);
         assertEquals(whitePlayerId, createGameDto.whitePlayerId());
         assertEquals(blackPlayerId, createGameDto.blackPlayerId());
     }
 
     @Test
     void createGameDtoShouldReturnNullForWhitePlayerIdWhenInitializedWithNull() {
+        var gameId = "gameId";
         var blackPlayerId = "blackPlayerId";
-        var createGameDto = new NewGameDto(null, blackPlayerId);
+        var createGameDto = new NewGameDto(gameId, null, blackPlayerId);
         assertNull(createGameDto.whitePlayerId());
         assertEquals(blackPlayerId, createGameDto.blackPlayerId());
     }
 
     @Test
     void createGameDtoShouldReturnNullForBlackPlayerIdWhenInitializedWithNull() {
+        var gameId = "gameId";
         var whitePlayerId = "whitePlayerId";
-        var createGameDto = new NewGameDto(whitePlayerId, null);
+        var createGameDto = new NewGameDto(gameId, whitePlayerId, null);
         assertEquals(whitePlayerId, createGameDto.whitePlayerId());
         assertNull(createGameDto.blackPlayerId());
     }
 
     @Test
     void createGameDtoShouldReturnNullForBothPlayerIdsWhenInitializedWithNull() {
-        var createGameDto = new NewGameDto(null, null);
+        var gameId = "gameId";
+        var createGameDto = new NewGameDto(gameId, null, null);
         assertNull(createGameDto.whitePlayerId());
         assertNull(createGameDto.blackPlayerId());
     }
